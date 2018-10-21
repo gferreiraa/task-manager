70bc5033de421ca0b883fb46a80dc4ea7eec7568
gferreira <gferreiraa@gmail.com>
2018-10-21 18:12:58 -0300

    Integration Angular and Firestore

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Task } from './models/task.model';
+import { AngularFirestore } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'task-manager';
+
+  tasks$: Observable<Task[]>;
+
+  constructor(
+    private db: AngularFirestore
+  ) {}
+
+  ngOnInit(): void {
+    this.tasks$ = this.db.collection<Task>('/tasks').valueChanges();
+  }
 }
