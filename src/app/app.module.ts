3a2dd8a15d8bfd9fcad97cf8f696a0d89b6e968a
gferreira <gferreiraa@gmail.com>
2018-10-21 20:45:14 -0300

    New components created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,30 @@ import { AppComponent } from './app.component';
 import { AngularFireModule } from '@angular/fire';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { environment } from './../environments/environment';
-import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatListModule } from '@angular/material/list';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+
+
+import { TaskItemComponent } from './task-item/task-item.component';
+import { MatLineModule } from '@angular/material/core';
+import { TaskListComponent } from './task-list/task-list.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TaskItemComponent,
+    TaskListComponent
   ],
   imports: [
     BrowserAnimationsModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFirestoreModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatListModule,
+    MatLineModule,
+    MatSlideToggleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
