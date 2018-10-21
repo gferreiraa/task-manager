c7571e1c8a11a45009c6bfd1f200ea5330cc8aa4
gferreira <gferreiraa@gmail.com>
2018-10-21 18:45:18 -0300

    Add angular-material and config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,22 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AngularFireModule } from '@angular/fire';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { environment } from './../environments/environment';
+import {MatToolbarModule} from '@angular/material/toolbar';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
+    BrowserAnimationsModule,
     AngularFireModule.initializeApp(environment.firebase),
-    AngularFirestoreModule
+    AngularFirestoreModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
