690279873eb633d9ea49c5e1928d2c02f9b9fc6e
gferreira <gferreiraa@gmail.com>
2018-10-21 21:45:22 -0300

    Update task service

--- src/app/task.service.ts
+++ src/app/task.service.ts
@@ -1,3 +1,4 @@
+import { Observable } from 'rxjs';
 import { Injectable } from '@angular/core';
 import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
 import { Task } from './models/task.model';
@@ -18,4 +19,29 @@ export class TaskService {
   private setTasks(): void {
     this.tasks = this.db.collection<Task>('/tasks');
   }
+
+  create(task: Task): Promise<void> {
+    const uid = this.db.createId();
+    return this.tasks.doc<Task>(uid)
+    .set({
+      uid,
+      title: task.title,
+      done: false
+    });
+  }
+
+  update(task: Task): Promise<void> {
+    return this.tasks.doc<Task>(task.uid)
+   .update(task);
+  }
+
+  delete(task: Task): Promise<void> {
+    return this.tasks.doc<Task>(task.uid)
+    .delete();
+  }
+
+  get(uid: string): Observable<Task> {
+    return this.tasks.doc<Task>(uid).valueChanges();
+  }
+
 }
